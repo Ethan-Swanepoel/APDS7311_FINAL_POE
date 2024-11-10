0000d65b26d5d211f33848dc8408a27540b3138b
Ethan-Swanepoel <ST10198049@vcconnect.edu.za>
2024-11-10 11:13:55 +0200

    server.mjs

---
+++ BACKEND/server.mjs
@@ -0,0 +1,44 @@
+import https from "http";
+import fs from "fs";
+import customers from "./routes/customer.mjs";
+import payments from "./routes/payment.mjs";
+import employee from "./routes/employee.mjs";
+import express from "express";
+import cors from "cors";
+
+const PORT = 3001;
+const app = express();
+
+const options = {
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
+    credentials: true
+}));
+
+
+app.use(express.json());
+
+//app.use((req, res, next) => {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Headers', '*');
+//    res.setHeader('Access-Control-Allow-Methods', '*');
+//    next();
+//});
+
+// Use the defined route handlers
+app.use("/customer", customers);
+app.route("/customer", customers);
+app.use("/payment", payments);
+app.route("/payment", payments);
+app.use("/employee", employee);
+app.route("/employee", employee);
+
+let server = https.createServer(options, app);
+console.log(`Server running on port ${PORT}`);
+server.listen(PORT);
